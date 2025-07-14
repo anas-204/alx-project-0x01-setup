304bb1e7824062fa2260b7129c717e25dc9f4126
Anas <anaselsheikh2004@gmail.com>
2025-07-15 00:47:38 +0300

    interfaces and components created

--- alx-project-0x01/interfaces/index.ts
+++ alx-project-0x01/interfaces/index.ts
@@ -59,5 +59,5 @@ export interface UserData {
 export interface UserModalProps {
   isOpen: boolean;
   onClose: () => void;
-  onSubmit: (user: UserData) => void;
+  onSubmit: (post: UserProps) => void;
 }
